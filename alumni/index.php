871d8708dad657334d62b70d554f04cc8cb45a96
Istiak <sifatistiak@gmail.com>
2021-02-08 01:18:13 +0600

    image height

--- alumni/index.php
+++ alumni/index.php
@@ -1,56 +1,58 @@
 <html>
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=yes">
-    <title>BUPAF</title>
-    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/css/Navigation-with-Button.css">
-    <link rel="stylesheet" href="../assets/css/styles.css">
-    <link rel="shortcut icon" href="../assets/img/logo2.png" type="image/png">
-    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=yes">
+  <title>BUPAF</title>
+  <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../assets/css/Navigation-with-Button.css">
+  <link rel="stylesheet" href="../assets/css/styles.css">
+  <link rel="shortcut icon" href="../assets/img/logo2.png" type="image/png">
+  <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
 
 </head>
 
 <body style="width:100%; height: 100%; margin: 0px; padding: 0px; overflow-x: hidden;">
   <div style="margin-left:12.5%;margin-right:12.5%;">
 
-<div>
-    <div class="container fixed-top">
+    <div>
+      <div class="container fixed-top">
 
         <nav class="navbar navbar-dark navbar-expand-md text-uppercase" style="height: 60px;">
           <div class="container-fluid">
-              <a class="navbar-brand" href="../" style="width: 20%;margin-left: 10%;">
+            <a class="navbar-brand" href="../" style="width: 20%;margin-left: 10%;">
               <img src="../assets/img/logo.png" width="100%" style="padding: -8px;margin-left: -61%;width: 137px;padding-bottom: -11px;padding-left: 1px;">
             </a>
             <button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse sticky-top" style="background:#2b2b2b" id="navcol-1">
-                    <ul class="nav navbar-nav mx-auto">
-                        <li class="nav-item" role="presentation"><a class="nav-link active" href="../news">NEWS</a></li>
-                        <li class="nav-item" role="presentation"><a class="nav-link active" href="../blog">BLOGS</a></li>
-                        <li class="nav-item" role="presentation"><a class="nav-link active" href="../event">EVENT</a></li>
-                        <li class="nav-item" role="presentation"><a class="nav-link active" href="../alumni">ALUMNI</a></li>
-                        <li class="nav-item" role="presentation"><a class="nav-link active" href="../publication">PUBLICATION</a></li>
-                        <li class="nav-item" role="presentation"><a class="nav-link active" href="../member">MEMBERS</a></li>
-                    </ul><a class="text-light login" href="../login" style="filter: brightness(153%);">SIGN IN</a></div>
+            <div class="collapse navbar-collapse sticky-top" style="background:#2b2b2b" id="navcol-1">
+              <ul class="nav navbar-nav mx-auto">
+                <li class="nav-item" role="presentation"><a class="nav-link active" href="../news">NEWS</a></li>
+                <li class="nav-item" role="presentation"><a class="nav-link active" href="../blog">BLOGS</a></li>
+                <li class="nav-item" role="presentation"><a class="nav-link active" href="../event">EVENT</a></li>
+                <li class="nav-item" role="presentation"><a class="nav-link active" href="../alumni">ALUMNI</a></li>
+                <li class="nav-item" role="presentation"><a class="nav-link active" href="../publication">PUBLICATION</a></li>
+                <li class="nav-item" role="presentation"><a class="nav-link active" href="../member">MEMBERS</a></li>
+              </ul><a class="text-light login" href="../login" style="filter: brightness(153%);">SIGN IN</a>
+            </div>
+          </div>
+
+        </nav>
+        <hr class="header">
       </div>
-
-</nav>
-<hr class="header">
     </div>
-</div>
-<!-- div class full body  -->
-<div class="col-md-12" style="margin-top:8%;">
+    <!-- div class full body  -->
+    <div class="col-md-12" style="margin-top:8%;">
 
-  <h3 style="width: 20%; color:#D1B055; font-family: 'Montserrat'; font-size: 2em; font-weight: bold;">Alumni</h3>
-
+      <h3 style="width: 20%; color:#D1B055; font-family: 'Montserrat'; font-size: 2em; font-weight: bold;">Alumni</h3>
 
 
-</div>
 
-  <?php
-  include 'alumni_page.php';
-  ?>
-<!-- <div class="col-md-3" style=" font-size: 14px; color: white; background-color:rgba(0, 0, 0, 0.2); border-radius:30px; margin:3%">
+    </div>
+
+    <?php
+    include 'alumni_page.php';
+    ?>
+    <!-- <div class="col-md-3" style=" font-size: 14px; color: white; background-color:rgba(0, 0, 0, 0.2); border-radius:30px; margin:3%">
     <div class="row">
                         <div class="col-md-4">
                             <img src="../assets/img/logo2.png" class="rounded-circle img-fluid" alt="" style="margin-top: 25%; margin-botom:25%;"/>
@@ -63,27 +65,27 @@
                         </div>
       </div>
     </div> -->
-<!-- </div> -->
+    <!-- </div> -->
 
-</div><!-- div class full body  -->
+  </div><!-- div class full body  -->
 
-<div class="row">
-  <div class="col-md-12">
+  <div class="row">
+    <div class="col-md-12">
 
-  <center>
-  <div style="width: 162px;"><img src="../assets/img/logo.png"  style="height: 40%;width: 100%; margin-top: 14px;"></div>
-</center>
-<hr class="footer">
-<center>
-<p>© 2019 All Rights Reserved | Developed by <a href="https://skoder.co">Skoder</a></p>
-</center>
-</div>
-</div>
+      <center>
+        <div style="width: 162px;"><img src="../assets/img/logo.png" style=" width: 10%; margin-top: 14px;"></div>
+      </center>
+      <hr class="footer">
+      <center>
+        <p>© 2019 All Rights Reserved | Developed by <a href="https://skoder.co">Skoder</a></p>
+      </center>
+    </div>
+  </div>
 
-</div>
-<script src="../assets/js/jquery.min.js"></script>
-<script src="../assets/bootstrap/js/bootstrap.min.js"></script>
-<!-- <script src="https://www.stadget.com/cdn/widget-init.min.js"></script> -->
+  </div>
+  <script src="../assets/js/jquery.min.js"></script>
+  <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+  <!-- <script src="https://www.stadget.com/cdn/widget-init.min.js"></script> -->
 </body>
 
 </html>
