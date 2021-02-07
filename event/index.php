66d7acd65062a82e392b4873903f1b4de69118d9
Istiak <sifatistiak@gmail.com>
2021-02-08 01:44:00 +0600

    Update index.php

--- event/index.php
+++ event/index.php
@@ -1,13 +1,17 @@
+<?php
+error_reporting(0);
+?>
 <html>
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=yes">
-    <title>BUPAF</title>
-    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/css/Navigation-with-Button.css">
-    <link rel="stylesheet" href="../assets/css/styles.css">
-    <link rel="shortcut icon" href="../assets/img/logo2.png" type="image/png">
-    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=yes">
+  <title>BUPAF</title>
+  <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../assets/css/Navigation-with-Button.css">
+  <link rel="stylesheet" href="../assets/css/styles.css">
+  <link rel="shortcut icon" href="../assets/img/logo2.png" type="image/png">
+  <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
 
 </head>
 
@@ -15,51 +19,51 @@
   <div style="margin-left:12.5%;margin-right:12.5%;">
 
     <div>
-    <?php
-    include('../include/header.php');
-     ?>
+      <?php
+      include('../include/header.php');
+      ?>
     </div>
 
-<!-- div class full body  -->
-<div class="col-md-12" style="margin-top:8%;">
+    <!-- div class full body  -->
+    <div class="col-md-12" style="margin-top:8%;">
 
-  <h3 style="width: 20%; color:#D1B055; font-family: 'Montserrat Bold';">EVENTS</h3>
+      <h3 style="width: 20%; color:#D1B055; font-family: 'Montserrat Bold';">EVENTS</h3>
 
 
-<div class="row" style="margin-top:3%; position: relative; text-align: center; color: white;">
+      <div class="row" style="margin-top:3%; position: relative; text-align: center; color: white;">
 
-<div class="col-md-4">
-<a href="../nao">
-<img src="../assets/img/slider1.jpg" class="col-md-12" alt="Responsive image" style="border-radius: 8%;">
-<br><br>
-<h6 style="font-weight:bold; color:#D1B055; ">NATIONAL ACCOUNTING OLYMPIAD</h6>
-<br>
-<p style="text-align: justify;">The name of the organization shall be "Bangladesh university of professionals accounting forum (BUPAF)" (hereinafter termed the "Forum"); also regarded as "BUP Accounting Forum". It came into force as an organization of the existing students and representing students from all running batches of Department of Accounting & Information Systems, Bangladesh University of Professionals. It comes into existence on 27th April, 2019 through the declaration of the committee by the faculty members of the Department of Accounting & Information Systems, Faculty of Business Studies, Bangladesh University of Professionals. </p>
-</a>
-</div>
+        <div class="col-md-4">
+          <a href="../nao">
+            <img src="../assets/img/slider1.jpg" class="col-md-12" alt="Responsive image" style="border-radius: 8%;">
+            <br><br>
+            <h6 style="font-weight:bold; color:#D1B055; ">NATIONAL ACCOUNTING OLYMPIAD</h6>
+            <br>
+            <p style="text-align: justify;">The name of the organization shall be "Bangladesh university of professionals accounting forum (BUPAF)" (hereinafter termed the "Forum"); also regarded as "BUP Accounting Forum". It came into force as an organization of the existing students and representing students from all running batches of Department of Accounting & Information Systems, Bangladesh University of Professionals. It comes into existence on 27th April, 2019 through the declaration of the committee by the faculty members of the Department of Accounting & Information Systems, Faculty of Business Studies, Bangladesh University of Professionals. </p>
+          </a>
+        </div>
 
-<div class="col-md-4">
-<img src="../assets/img/slider2.jpg" class="col-md-12" alt="Responsive image" style="border-radius: 8%;">
-<br><br>
-<h6 style="font-weight:bold; color:#D1B055; ">TRANING SESSION </h6>
-<br>
-<p style="text-align: justify;">The name of the organization shall be "Bangladesh university of professionals accounting forum (BUPAF)" (hereinafter termed the "Forum"); also regarded as "BUP Accounting Forum". It came into force as an organization of the existing students and representing students from all running batches of Department of Accounting & Information Systems, Bangladesh University of Professionals. It comes into existence on 27th April, 2019 through the declaration of the committee by the faculty members of the Department of Accounting & Information Systems, Faculty of Business Studies, Bangladesh University of Professionals. </p>
-</div>
+        <div class="col-md-4">
+          <img src="../assets/img/slider2.jpg" class="col-md-12" alt="Responsive image" style="border-radius: 8%;">
+          <br><br>
+          <h6 style="font-weight:bold; color:#D1B055; ">TRAINING SESSION </h6>
+          <br>
+          <p style="text-align: justify;">The name of the organization shall be "Bangladesh university of professionals accounting forum (BUPAF)" (hereinafter termed the "Forum"); also regarded as "BUP Accounting Forum". It came into force as an organization of the existing students and representing students from all running batches of Department of Accounting & Information Systems, Bangladesh University of Professionals. It comes into existence on 27th April, 2019 through the declaration of the committee by the faculty members of the Department of Accounting & Information Systems, Faculty of Business Studies, Bangladesh University of Professionals. </p>
+        </div>
 
-<div class="col-md-4">
-<img src="../assets/img/slider3.jpg" class="col-md-12" alt="Responsive image" style="border-radius: 8%;">
-<br><br>
-<h6 style="font-weight:bold; color:#D1B055;  ">SEMINERS</h6>
-<br>
-<p style="text-align: justify;">The name of the organization shall be "Bangladesh university of professionals accounting forum (BUPAF)" (hereinafter termed the "Forum"); also regarded as "BUP Accounting Forum". It came into force as an organization of the existing students and representing students from all running batches of Department of Accounting & Information Systems, Bangladesh University of Professionals. It comes into existence on 27th April, 2019 through the declaration of the committee by the faculty members of the Department of Accounting & Information Systems, Faculty of Business Studies, Bangladesh University of Professionals. </p>
-</div>
+        <div class="col-md-4">
+          <img src="../assets/img/slider3.jpg" class="col-md-12" alt="Responsive image" style="border-radius: 8%;">
+          <br><br>
+          <h6 style="font-weight:bold; color:#D1B055;  ">SEMINARS</h6>
+          <br>
+          <p style="text-align: justify;">The name of the organization shall be "Bangladesh university of professionals accounting forum (BUPAF)" (hereinafter termed the "Forum"); also regarded as "BUP Accounting Forum". It came into force as an organization of the existing students and representing students from all running batches of Department of Accounting & Information Systems, Bangladesh University of Professionals. It comes into existence on 27th April, 2019 through the declaration of the committee by the faculty members of the Department of Accounting & Information Systems, Faculty of Business Studies, Bangladesh University of Professionals. </p>
+        </div>
 
-</div>
+      </div>
 
-<?php
-include 'pub_page.php';
-?>
-<!-- <div class="row" style=" font-size: 14px; color: white; margin-top:5%; background-color:rgb(0,0,0); opacity:0.5;">
+      <?php
+      include 'pub_page.php';
+      ?>
+      <!-- <div class="row" style=" font-size: 14px; color: white; margin-top:5%; background-color:rgb(0,0,0); opacity:0.5;">
     <div class="col-md-2">
       <img class="col-md-12 rounded img-fluid" src="../assets/img/logo2.png">
     </div>
@@ -72,25 +76,24 @@ include 'pub_page.php';
 </div> -->
 
 
-</div><!-- div class full body  -->
+    </div><!-- div class full body  -->
 
-<div class="row">
-  <div class="col-md-12">
+    <div class="row">
+      <div class="col-md-12">
 
-  <center>
-  <div><img src="../assets/img/logo.png"  style="width: 10%; margin-top: 14px;"></div>
-</center>
-<hr class="footer">
-<center>
-<p>© 2019 - 2021 All Rights Reserved | Developed by <a href="https://skoder.co">Skoder</a></p>
-</center>
-</div>
-</div>
+        <center>
+          <div><img src="../assets/img/logo.png" style="width: 10%; margin-top: 14px;"></div>
+        </center>
+        <hr class="footer">
+        <center>
+          <p>© 2019 - 2021 All Rights Reserved | Developed by <a href="https://skoder.co">Skoder</a></p>
+        </center>
+      </div>
+    </div>
 
-</div>
-<script src="../assets/js/jquery.min.js"></script>
-<script src="../assets/bootstrap/js/bootstrap.min.js"></script>
-<!-- <script src="https://www.stadget.com/cdn/widget-init.min.js"></script> -->
+  </div>
+  <script src="../assets/js/jquery.min.js"></script>
+  <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
 </body>
 
 </html>
