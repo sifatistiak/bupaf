17dbda275e65d1c686d4cf2de664c3984c0de090
Istiak <sifatistiak@gmail.com>
2021-02-27 01:49:41 +0600

    Publication list

--- publication/index.php
+++ publication/index.php
@@ -1,112 +1,99 @@
 <html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=yes">
-    <title>BUPAF</title>
-    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/css/Navigation-with-Button.css">
-    <link rel="stylesheet" href="../assets/css/styles.css">
-    <link rel="shortcut icon" href="../assets/img/logo2.png" type="image/png">
-    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
 
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=yes">
+  <title>BUPAF</title>
+  <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../assets/css/Navigation-with-Button.css">
+  <link rel="stylesheet" href="../assets/css/styles.css">
+  <link rel="shortcut icon" href="../assets/img/logo2.png" type="image/png">
+  <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+
+  <style>
+    .card {
+      background-color: black;
+      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+      transition: 0.1s;
+      width: 100%;
+    }
+
+    .card:hover {
+      box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+    }
+
+    .container {
+      padding: 2px 16px;
+    }
+  </style>
 </head>
 
-<body style="width:100%; height: 100%; margin: 0px; padding: 0px; font-family: 'Montserrat'; overflow-x: hidden;">
+<body style="width:100%; height: 100%; margin: 0px; padding: 0px; overflow-x: hidden;">
   <div style="margin-left:12.5%;margin-right:12.5%;">
 
-<div>
-    <div class="container fixed-top">
+    <div>
+      <?php
+      include '../include/config.php';
 
-        <nav class="navbar navbar-dark navbar-expand-md text-uppercase" style="height: 60px;">
-          <div class="container-fluid">
-              <a class="navbar-brand" href="../" style="width: 20%;margin-left: 10%;">
-              <img src="../assets/img/logo.png" width="100%" style="padding: -8px;margin-left: -61%;width: 137px;padding-bottom: -11px;padding-left: 1px;">
-            </a>
-            <button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse sticky-top" style="background:#2b2b2b" id="navcol-1">
-                    <ul class="nav navbar-nav mx-auto">
-                        <li class="nav-item" role="presentation"><a class="nav-link active" href="../news">NEWS</a></li>
-                        <li class="nav-item" role="presentation"><a class="nav-link active" href="../blog">BLOGS</a></li>
-                        <li class="nav-item" role="presentation"><a class="nav-link active" href="../event">EVENT</a></li>
-                        <li class="nav-item" role="presentation"><a class="nav-link active" href="../alumni">ALUMNI</a></li>
-                        <li class="nav-item" role="presentation"><a class="nav-link active" href="../publication">PUBLICATION</a></li>
-                        <li class="nav-item" role="presentation"><a class="nav-link active" href="../member">MEMBERS</a></li>
-                    </ul><a class="text-light login" href="../login" style="filter: brightness(153%);">SIGN IN</a></div>
+      include('../include/header.php');
+      ?>
+    </div>
+    <!-- div class full body  -->
+    <div class="col-md-12" style="margin-top:8%;">
+
+      <h3 style="width: 20%; color:#D1B055; font-family: 'Montserrat Bold';">Publications</h3>
+
+
+      <!-- <div class="row text-center" style="margin-top:3%; position: relative; text-align: left; color: white;"> -->
+
+
+      <!-- <img src="../assets/img/slider1.png" class="col-md-12" alt="Responsive image"> -->
+      <!-- height:250px; style e add hbe -->
+      <!-- <div style="position: absolute; bottom: 4%; background-color:rgba(128,128,128,0.5); margin-left:5%; width:90%; padding:2%">
+          <h3>Publications</h3>
+          Department of AIS inaugurates BUP Accounting Forum
+        </div> -->
+      <!-- </div> -->
+
+      <div class="row" style="margin-top:1rem;">
+        <?php
+        $query = "select * from `publications` order by id desc ";
+        $result = mysqli_query($con, $query);
+        if (mysqli_num_rows($result) > 0) {
+          while ($row = mysqli_fetch_object($result)) {
+        ?>
+            <div class="col-md-4" style="padding: 1rem;">
+              <div class="card">
+                <div class="container">
+                  <h4> <a style="color:white;" href="<?php echo $row->link; ?>"><?php echo $row->title;  ?></a> </h4>
+                  <h6> <a style="color:lightblue;" href="<?php echo $row->link; ?>"><?php echo $row->link; ?></a> </h6>
+                </div>
+              </div>
+            </div>
+        <?php }
+        } ?>
       </div>
 
-</nav>
-<hr class="header">
+
+    </div><!-- div class full body  -->
+
+    <div class="row">
+      <div class="col-md-12">
+
+        <center>
+          <div><img src="../assets/img/logo.png" style="width: 10%; margin-top: 14px;"></div>
+        </center>
+        <hr class="footer">
+        <center>
+          <p>© 2019 - 2021 All Rights Reserved | Developed by <a href="https://skoder.co">Skoder</a></p>
+        </center>
+      </div>
     </div>
-</div>
-<!-- div class full body  -->
-<div class="col-md-12" style="margin-top:8%;">
 
-  <h3 style="width: 20%; color:#D1B055; font-family: 'Montserrat'; font-size: 2em; font-weight: bold;">PUBLICATIONS</h3>
-
-
-<div class="row" style="margin-top:3%; position: relative; text-align: center; color: white;">
-
-<div class="col-md-4">
-<img src="../assets/img/slider1.png" class="col-md-12" alt="Responsive image" style="border-radius: 14%;">
-<br><br>
-<h6 style="font-weight:bold; color:#D1B055; ">NATIONAL ACCOUNTING OLYMPIAD</h6>
-<br>
-<p style="text-align: justify;">The name of the organization shall be "Bangladesh university of professionals accounting forum (BUPAF)" (hereinafter termed the "Forum"); also regarded as "BUP Accounting Forum". It came into force as an organization of the existing students and representing students from all running batches of Department of Accounting & Information Systems, Bangladesh University of Professionals. It comes into existence on 27th April, 2019 through the declaration of the committee by the faculty members of the Department of Accounting & Information Systems, Faculty of Business Studies, Bangladesh University of Professionals. </p>
-</div>
-
-<div class="col-md-4">
-<img src="../assets/img/slider2.png" class="col-md-12" alt="Responsive image" style="border-radius: 14%;">
-<br><br>
-<h6 style="font-weight:bold; color:#D1B055; ">TRANING SESSION </h6>
-<br>
-<p style="text-align: justify;">The name of the organization shall be "Bangladesh university of professionals accounting forum (BUPAF)" (hereinafter termed the "Forum"); also regarded as "BUP Accounting Forum". It came into force as an organization of the existing students and representing students from all running batches of Department of Accounting & Information Systems, Bangladesh University of Professionals. It comes into existence on 27th April, 2019 through the declaration of the committee by the faculty members of the Department of Accounting & Information Systems, Faculty of Business Studies, Bangladesh University of Professionals. </p>
-</div>
-
-<div class="col-md-4">
-<img src="../assets/img/slider3.png" class="col-md-12" alt="Responsive image" style="border-radius: 14%;">
-<br><br>
-<h6 style="font-weight:bold; color:#D1B055;  ">SEMINERS</h6>
-<br>
-<p style="text-align: justify;">The name of the organization shall be "Bangladesh university of professionals accounting forum (BUPAF)" (hereinafter termed the "Forum"); also regarded as "BUP Accounting Forum". It came into force as an organization of the existing students and representing students from all running batches of Department of Accounting & Information Systems, Bangladesh University of Professionals. It comes into existence on 27th April, 2019 through the declaration of the committee by the faculty members of the Department of Accounting & Information Systems, Faculty of Business Studies, Bangladesh University of Professionals. </p>
-</div>
-
-</div>
-
-<?php
-include 'pub_page.php';
-?>
-<!-- <div class="row" style=" font-size: 14px; color: white; margin-top:5%; background-color:rgb(0,0,0); opacity:0.5;">
-    <div class="col-md-2">
-      <img class="col-md-12 rounded img-fluid" src="../assets/img/logo2.png">
-    </div>
-    <div class="col-md-10">
-      <h6 style="color:#D1B055; ">Title</h6>
-      <p style="text-align: left;">Text</p>
-      <br>
-      <p style="text-align: left;">Tags: </p>
-    </div>
-</div> -->
-
-
-</div><!-- div class full body  -->
-
-<div class="row">
-  <div class="col-md-12">
-
-  <center>
-  <div><img src="../assets/img/logo.png"  style="width: 10%; margin-top: 14px;"></div>
-</center>
-<hr class="footer">
-<center>
-<p>© 2019 - 2021 All Rights Reserved | Developed by <a href="https://skoder.co">Skoder</a></p>
-</center>
-</div>
-</div>
-
-</div>
-<script src="../assets/js/jquery.min.js"></script>
-<script src="../assets/bootstrap/js/bootstrap.min.js"></script>
-<!-- <script src="https://www.stadget.com/cdn/widget-init.min.js"></script> -->
+  </div>
+  <script src="../assets/js/jquery.min.js"></script>
+  <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+  <!-- <script src="https://www.stadget.com/cdn/widget-init.min.js"></script> -->
 </body>
 
 </html>
